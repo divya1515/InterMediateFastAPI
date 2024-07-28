59e1e467cfa061c73954fa26643320a714b00c81
divya1515 <divya.kaushik2244@gmail.com>
2024-07-28 16:02:23 +0530

    imp

--- myapi.py
+++ myapi.py
@@ -10,6 +10,8 @@ def index():
 def show():
     return {"all unpublished blogs"}
 
+# As below path is blogs/id that is dynamic route ..if i had written this dynamic route above the route /blogs/unpublished  then whenever the user goes to blogs/unpublished then it would give error as Fast api reads routing line by ine ...it would take unpublished as {id} dynamic id thus this function of /blogs/{id} will execute and then give type error that id is not integer
+
 @app.get('/blogs/{id}')
 def show(id:int):
     return {"blog":{id}}
