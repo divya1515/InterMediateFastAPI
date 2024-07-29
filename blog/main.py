4627356574cdbc514dad90669fce6dcf3a57cb40
divya1515 <divya.kaushik2244@gmail.com>
2024-07-29 09:57:13 +0530

    database connection

--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI 
-from . import schemas
+from . import schemas , models
+from .database import engine 
+
 
 app=FastAPI()
 
+models.Base.metadata.create_all(engine)
 
 @app.post('/blog')
 def create(blog:schemas.Blog):
-    return request
+    return blog
